a433c9db3ac50d077b2fbfe840df612635581840
Diogo Carbonera Luvizon <diogo.luvizon@ensitec.com.br>
2013-02-19 00:07:04 -0300

    not depending to be run from home
    	The install script was changed to be run from any where.

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-dir=~/dotfiles
-backupdir=~/dotfiles/backup
-files=`ls $dir`
-mkdir -p $backupdir
+DIR=`pwd`
+BACKUPDIR=$DIR/backup
+FILES=`ls $DIR -I *.sh`
+mkdir -p $BACKUPDIR
 
-pushd $dir
-	for file in $files; do
-		mv ~/.$file $backupdir
-		ln -s $dir/$file ~/.$file
+pushd $DIR
+	for file in $FILES; do
+		if [ -e "$HOME/.$file" ]; then
+			mv $HOME/.$file $BACKUPDIR
+		fi
+		ln -s $DIR/$file $HOME/.$file
 	done
 popd
